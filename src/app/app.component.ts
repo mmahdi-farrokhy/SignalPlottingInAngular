53cc63d0725462b66cff8bfd85b8c1d63f9e5679
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-10-06 16:08:01 +0330

    - project initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material.module';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MaterialModule,
+    NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'real-time-plotting-app';
+  channels = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  selectedChannels = 1;
+
+  startDrawing() {
+    console.log(`Selected number of channels: ${this.selectedChannels}`);
+  }
 }
