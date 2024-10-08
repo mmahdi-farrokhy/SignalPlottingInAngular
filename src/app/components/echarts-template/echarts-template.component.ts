c91abb045e3c91cadf0b4f7b41fb26db12955b7b
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-10-08 09:34:23 +0330

    - created component EChartsTemplateComponent

---
+++ src/app/components/echarts-template/echarts-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-echarts-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './echarts-template.component.html',
+  styleUrl: './echarts-template.component.css'
+})
+export class EChartsTemplateComponent {
+
+}
