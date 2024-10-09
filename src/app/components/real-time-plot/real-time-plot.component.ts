ef656937d2426b278120de533deca27a6aec7c04
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-10-09 08:40:38 +0330

    - added ECharts component to main page

--- src/app/components/real-time-plot/real-time-plot.component.ts
+++ src/app/components/real-time-plot/real-time-plot.component.ts
@@ -3,11 +3,12 @@ import uPlot from 'uplot';
 import { UPlotTemplateComponent } from '../uplot-template/uplot-template.component';
 import { PlotType } from '../../plot-type';
 import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { EChartsTemplateComponent } from "../echarts-template/echarts-template.component";
 
 @Component({
   selector: 'app-real-time-plot',
   standalone: true,
-  imports: [UPlotTemplateComponent, NgIf, NgSwitch, NgSwitchCase],
+  imports: [UPlotTemplateComponent, NgIf, NgSwitch, NgSwitchCase, EChartsTemplateComponent],
   templateUrl: './real-time-plot.component.html',
   styleUrl: './real-time-plot.component.css'
 })
